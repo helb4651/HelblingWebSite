b70f2b78ce07bd16fbd9cfbc7f6d3b1a2ae405c6
Cody Helbling <chelbling@decagon.com>
2016-09-10 13:37:22 -0700

    Initial commit

---
+++ ContentBlock/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class ContentBlock(models.Model):
+    image_locaiton = 'test_image'
+    title = 'test_title'
+    color = 'test_color'
---
+++ ContentBlock/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+
+# Create your views here.
+from django.template import loader
+from django.views.generic import TemplateView
+
+
+def hello(request):
+    return HttpResponse(loader.get_template("hello.html").render())
+
+def momo(request):
+    return render_to_response('index.html')
