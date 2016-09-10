4fd749513c74e1fa1b240a68b0155cddcd2bc3d4
Cody Helbling <chelbling@decagon.com>
2016-09-10 15:56:06 -0700

    Added bootstrap requirements.

--- CodyHelbling/settings.py
+++ CodyHelbling/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'djangorestframework',
-    # 'markdown',
-    # 'django-filter',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
